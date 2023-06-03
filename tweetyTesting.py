9d941e3a191cc9998408567e810bec37e29d9974
kyrillPysarenko <s0582947@htw-berlin.de>
2023-06-03 18:10:14 +0200

    sentiment analysis added

--- tweetyTesting.py
+++ tweetyTesting.py
@@ -6,6 +6,9 @@ from datetime import datetime
 import datetime as dt
 import yfinance as yf
 from sklearn.feature_extraction.text import CountVectorizer
+from textblob import TextBlob
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
 
 app = Twitter()
 
@@ -16,27 +19,27 @@ def create_dataframe_from_tweets(tweets: List[Tweet]) -> pd.DataFrame:
             continue
         data.append({
             "id": tweet.id,
-            "text": tweet.text,
+            "text_tweet": tweet.text,
             "author": tweet.author.username,
             "created_at": tweet.date,
             "views": tweet.views,
             "length": len(tweet.text)
         })
-    df = pd.DataFrame(data, columns=["id", "text", "author", "date", "views", "created_at"])
+    df = pd.DataFrame(data, columns=["id", "text_tweet", "author", "date", "views", "created_at"])
     df.set_index("id", inplace=True)
     if df.empty:
         return df
     df = df[df.created_at.dt.date > datetime.now().date() - pd.to_timedelta("365day")]
     return df
 
-all_tweets = app.get_tweets(username= "elonmusk", pages=100)
+all_tweets = app.get_tweets(username= "VitalikButerin", pages=100)
 tweet_df = create_dataframe_from_tweets(all_tweets)
 print(create_dataframe_from_tweets(all_tweets))
 
 #Split and Count Tweets words
 count_vect = CountVectorizer(min_df = 1)
-X = count_vect.fit_transform(tweet_df["text"])
-tweetTxt = tweet_df["text"]
+X = count_vect.fit_transform(tweet_df["text_tweet"])
+tweetTxt = tweet_df["text_tweet"]
 dtm = pd.DataFrame(X.toarray())
 dtm.columns = count_vect.get_feature_names_out()
 data_dtm = pd.concat([tweet_df.reset_index(drop=True), dtm], axis=1)
@@ -44,7 +47,6 @@ data_dtm = pd.concat([tweet_df.reset_index(drop=True), dtm], axis=1)
 #leave out the time of the tweets DATE column
 data_dtm['created_at'] = pd.to_datetime(data_dtm['created_at'].apply(datetime.date))
 data_dtm = data_dtm.drop('date', axis=1)
-
 
 
 #CRYPTO PRICES
@@ -62,3 +64,9 @@ data_merged = pd.merge(data_dtm, eth_data[['return_prediction']], how='left', le
 data_merged = data_merged.dropna()
 data_merged = data_merged.reset_index(drop=True)
 data_merged['action'] = data_merged['return_prediction'].apply(lambda x: 'buy' if x > 0.002 else 'sell')
+
+#sentiment
+sid = SentimentIntensityAnalyzer()
+tweet_df['sentiment'] = tweet_df['text_tweet'].apply(lambda x: sid.polarity_scores(x))
+print(tweet_df[['text', 'sentiment']])
+
