fd03ee9b086de931683bc84ef12ece0d045ef13d
kyrillPysarenko <s0582947@htw-berlin.de>
2023-06-12 11:06:43 +0200

    LSTM on ETHUSD data

--- LSTM_first_test.py
+++ LSTM_first_test.py
@@ -15,10 +15,20 @@ start = dt.datetime(2016, 1, 1)
 end = dt.datetime.now()
 
 data = yf.download(tickers=f'{crypto_currency}-{against_currency}', period='7d', interval='1m')
+data = pd.read_csv('ETHUSD_1.csv')
+
+data.rename(columns={"1438956180": "Date", "3.0": "Open","3.0.1": "High", "3.0.2": "Low", "3.0.3": "Close", "81.85727776": "Volume", "2": "Trades"}, inplace=True)
+print(data.columns)
+data['Date'] = pd.to_datetime(data['Date'], unit='s')
+print(data.iloc[0, 1])
+rturn = data.iloc[0,4]-data.iloc[0,1]
+print(rturn)
+
+data['Return'] = data['Close'].shift(-1) - data['Close']
 
 # Prepare Data
 scaler = MinMaxScaler(feature_range=(0, 1))
-scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+scaled_data = scaler.fit_transform(data['Return'].values.reshape(-1, 1))
 
 prediction_mins = 60
 
@@ -26,7 +36,7 @@ x_train, y_train = [], []
 
 for x in range(prediction_mins, len(scaled_data)):
     x_train.append(scaled_data[x - prediction_mins:x, 0])
-    y_train.append(scaled_data[x, 0])
+    y_train.append(data.iloc[x, -1])
 
 x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
