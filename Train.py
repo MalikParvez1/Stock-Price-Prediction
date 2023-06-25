f36e94f1bf5116e9d2bca86e38941df4b252be49
kyrillPysarenko <s0582947@htw-berlin.de>
2023-06-25 13:57:27 +0200

    train file cont

--- Train.py
+++ Train.py
@@ -6,6 +6,13 @@ import pandas_ta as ta
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 
+import tensorflow as tf
+import keras
+from keras import optimizers
+from keras.callbacks import History
+from keras.models import Model
+from keras.layers import Dense, Dropout, LSTM , Input, Activation, concatenate
+
 df = mergeDataframes()
 
 #Adding technical indicators
@@ -20,13 +27,11 @@ print(df['target'], df['Close'])
 sc = MinMaxScaler(feature_range=(0,1))
 data_set_scaled = sc.fit_transform(df)
 
-pd.to_numeric(df['views'])
-
+#drop string data
 df.drop('author', axis=1, inplace=True)
 df.drop('Date', axis=1, inplace=True)
 df.drop('created_at', axis=1, inplace=True)
 df.drop('views', axis=1, inplace=True)
-
 
 X = []
 
@@ -54,7 +59,16 @@ print(y.shape)
 
 #split data into train and test
 splitlimit = int(len(X)*0.8)
-print(splitlimit)sploi
+print(splitlimit)
 X_train, X_test = X[:splitlimit], X[splitlimit:]
 y_train, y_test = y[:splitlimit], y[splitlimit:]
 
+#Model
+lstm_input = Input(shape=(backcandles, total_columns), name = 'lstm_input')
+inputs = LSTM(150, name='first_layer')(lstm_input)
+inputs = Dense(1, name='dense_layer')(inputs)
+output = Activation('linear', name='output')(inputs)
+model = Model(inputs=lstm_input, output=output)
+adam = optimizers.Adam()
+mdoel.compile(optimizers=adam, loss='mse')
+model.fit(x=X_train, y=y_train, batch_size=15, epochs=30, shuffle=True, validation_split=0.1)
